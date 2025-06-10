dec0884cca1f5b6d0d2caa1f99fa6dc35b49d0b6
Chrysa Pratikaki <chrisaprat@gmail.com>
2025-06-10 15:27:41 +0300

    Update misc.py

--- util/misc.py
+++ util/misc.py
@@ -316,7 +316,7 @@ def load_model(args, model_without_ddp, optimizer, loss_scaler):
             checkpoint = torch.hub.load_state_dict_from_url(
                 args.resume, map_location='cpu', check_hash=True)
         else:
-            checkpoint = torch.load(args.resume, map_location='cpu')
+            checkpoint = torch.load(args.resume, map_location='cpu', weights_only=False)
             # del checkpoint['model']['head.weight']
             # del checkpoint['model']['head.bias']
         model_without_ddp.load_state_dict(checkpoint['model'], strict=False)
